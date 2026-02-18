ab9197879c3a0c8cab720ad1e38876bd78b7e840
Andrei Laurynovich <104096994+Andrules@users.noreply.github.com>
2026-02-18 21:33:13 +0000

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # eslint-default-config
-I hope this files will help you to avoid this pain when you trying to create an app, but you spend a lot of hours to create ESLint and Prettier configs
+I hope this files will help you to avoid the pain when you trying to create an app, but you spend a lot of hours to create a proper ESLint configuration for the fisrt (or second) time in your life. 
+
+# About settings.json
+I strongly recommend you to create ".vscode/" directory on a highest level of project, right nearby package.json file. Inside this directory create settings.json file. What code should be inside you'll find in my example. 
+
+
+# About .eslingignore
+This file would help you to forget about unnecessary folders and files
+I didn't add it in this repository because it's up to project, but commonly i saw those lines in .eslintignore file
+
+# Just copy code below and paste in .eslintignore file (create before file if it still absent)
+
+build
+node_modules
+*.d.ts
+
+
---
+++ eslint.config.js
@@ -0,0 +1,90 @@
+// Please CHECK README.md before inject this file in project
+// Just use or extend, it's up to you
+
+//Please read this imports and use your package manager to install necessary node_modules
+import js from '@eslint/js'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import prettier from 'eslint-plugin-prettier'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import tseslint from 'typescript-eslint'
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended, //remove this line if you do not use TypeScript
+
+  {
+    files: ['**/*.{jsx,tsx}'],
+    plugins: {
+      react,
+      'react-hooks': reactHooks
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    },
+    rules: {
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn'
+    }
+  },
+
+  // General project rules
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    plugins: {
+      prettier,
+      'simple-import-sort': simpleImportSort // sorting-imports-plugin rules (trust me, this plugin really make sense)
+    },
+    rules: {
+      'no-console': 'warn',
+      'no-debugger': 'error',
+
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+
+      'no-unused-vars': 'off', // set 'on' if you do not use TS in your project
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_'
+        }
+      ],
+      '@typescript-eslint/consistent-type-imports': [
+        'warn',
+        {
+          prefer: 'type-imports'
+        }
+      ],
+
+      // IMPORTANT !!!
+      // prepare your vscode and turn off prettier extension for avoid conflicts between this formatter tool and global settings.json of VSCode.
+      // also recommend you to add /.vscode directory in root of your project and add some rules to turn on "onSave formatting"
+
+      // Prettier plugin configuration
+      'prettier/prettier': [
+        'error',
+        {
+          semi: false,
+          singleQuote: true,
+          jsxSingleQuote: true,
+          printWidth: 80,
+          tabWidth: 2,
+          endOfLine: 'auto',
+          trailingComma: 'none'
+        },
+        {
+          usePrettierrc: false // Preffer to use this settting not .prettierrc file
+        }
+      ]
+    }
+  },
+  // turn off conflict rules of ESLint
+  eslintConfigPrettier
+]
